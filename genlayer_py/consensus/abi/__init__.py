32061544214ac3f9af59720dd4197adcc06ce8aa
Daniel Rojas <danielrojasc888@gmail.com>
2025-06-13 12:51:32 -0500

    fix: improve reliability

--- genlayer_py/consensus/abi/__init__.py
+++ genlayer_py/consensus/abi/__init__.py
@@ -1,15 +1,18 @@
 import json
 import importlib.resources
 
-consensus_data_abi_path = importlib.resources.files("genlayer_py").joinpath(
-    "consensus/abi/consensus_data_abi.json"
-)
-consensus_main_abi_path = importlib.resources.files("genlayer_py").joinpath(
-    "consensus/abi/consensus_main_abi.json"
-)
-
-with open(consensus_data_abi_path, "r") as f:
+with importlib.resources.as_file(
+    importlib.resources.files("genlayer_py.consensus.abi").joinpath(
+        "consensus_data_abi.json"
+    )
+) as path, open(path, "r", encoding="utf-8") as f:
     CONSENSUS_DATA_ABI = json.load(f)
 
-with open(consensus_main_abi_path, "r") as f:
+with importlib.resources.as_file(
+    importlib.resources.files("genlayer_py.consensus.abi").joinpath(
+        "consensus_main_abi.json"
+    )
+) as path, open(path, "r", encoding="utf-8") as f:
     CONSENSUS_MAIN_ABI = json.load(f)
+
+__all__ = ["CONSENSUS_DATA_ABI", "CONSENSUS_MAIN_ABI"]
